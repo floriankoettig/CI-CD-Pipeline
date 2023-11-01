e5099823037da584ea74a9ffedf39a52c3100174
Köttig <Florian.Koettig@keb.de>
2023-11-01 11:58:39 +0100

    IBAN/IBANTest

---
+++ app/src/test/java/ci/cd/pipeline/IBANTest.java
@@ -0,0 +1,17 @@
+package ci.cd.pipeline;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class IBANTest {
+    @Test void IBANDE() {
+        String myIBAN = "DE68210501700012345678";
+        IBAN classUnderTest = new IBAN(myIBAN);
+        assertEquals(classUnderTest.check(myIBAN), false);
+    }
+
+    @Test void IBANWrong() {
+        String myIBAN = "ZZ68210501700012345678";
+        IBAN classUnderTest = new IBAN(myIBAN);
+        assertEquals(classUnderTest.check(myIBAN), false);
+    }
+}
